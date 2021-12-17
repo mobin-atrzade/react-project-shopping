b681b9f126e59592aa6f284532259f10ca7cab16
Mobin Atrzadeh <mbynatrzade@gmail.com>
2021-12-17 15:54:03 +0330

    add loginServise

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '../../common/Input';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import './signup.css'
 import { Link } from 'react-router-dom';
+import { signupUser } from './../../services/signupService';
 
 
 const initialValues = {
@@ -13,25 +14,41 @@ const initialValues = {
     password: "",
     passWordConfirm: ""
 }
-const onSubmit = (values) => {
-    console.log(values);
-    // axios.post('http://localhost:3001/users', values)
-    //     .then(res => console.log(res.data))
-    //     .catch(err => console.log(err))
-}
+
+
 const validationSchema = yup.object({
     name: yup.string().required('name is required').min(5, 'Name length is not valid'),
     email: yup.string().email('Invalid email format').required('email is required'),
     phoneNumber: yup.string().required('Phone number is required').matches(/^[0-9]{11}$/, 'Invalid Phone Number').nullable(),
-    password: yup.string().required('password is required')
-        .matches(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
-        ),
+    password: yup.string().required('password is required'),
+    // .matches(
+    //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+    //     "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    // ),
     passWordConfirm: yup.string().required('Password Confirmation is Required').oneOf([yup.ref('password'), null], 'Passwords must match')
 })
 
 const SignupForm = () => {
+    const [error, setError] = useState(null)
+    const onSubmit = async (values) => {
+        // console.log(values);
+        const { name, email, phoneNumber, password } = values;
+        const userData = {
+            name,
+            email,
+            phoneNumber,
+            password
+        }
+        try {
+            const { data } = await signupUser(userData);
+            console.log(data);
+        } catch (error) {
+            if (error.response && error.response.data.message) {
+                setError(error.response.data.message)
+            }
+        }
+
+    }
     const formik = useFormik(
         {
             initialValues,
@@ -56,6 +73,7 @@ const SignupForm = () => {
                     className="btn primary">
                     Signup
                 </button>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
                 <Link to="/login">
                     <p style={{ marginTop: '15px' }}>Already login ?</p>
                 </Link>
